aeba11d5132a5498ee2150fb7a38c0dac067eae1
mohamed-ali-source <git01551415200@gmail.com>
2024-11-19 14:39:20 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// document.getElementById("demo").innerHTML="Hello Mohamed";
+// document.getElementById("demo").innerText="Hello Salah";
+// console.log("Hello Mohamed");
+// console.log(document.getElementById("demo").innerText);
+// console.log(document.getElementById("demo").innerHTML);
+
+// // window.alert("Hello Mohamed");
+
+// // document.getElementById("demo").innerHTML="<p>hhhhhhh</p>";
+// // document.getElementById("demo").innerText="<p>hhhhhhh</p>";
+// var firstName=Number(window.prompt("fist name"));  //prompt==>string or null
+// var lastName=+(window.prompt("last name"));
+// console.log(firstName);
+// console.log(lastName);
+
+// var res= firstName + lastName;
+// console.log(res)
+
+// var age= res / 2
+// console.log(age);
+//-----------------------------------------------------
+// var userRole="editor";
+// if(userRole=="admin"){
+//     console.log("you can do anything");
+    
+// }else if(userRole=="editor"){
+//     console.log("you can edit");
+// }
+// else if(userRole=="moderator"){
+//     console.log("you can moderate");
+// }
+// else if(userRole=="user"){
+//     console.log("you can use");
+// }
+// else{
+//     console.log("invalid input");
+// }
